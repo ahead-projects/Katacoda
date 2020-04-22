a71059cfb8e644803fedcd6f8c5bc741b0ab9fe3
Bill Wang <bill.wang>
2020-04-22 18:07:34 +1000

    terraform-aws

--- terraform-aws/assets/main.tf
+++ terraform-aws/assets/main.tf
@@ -1,6 +1,14 @@
+# set variables
+variable "name" {
+  default= "demo"
+}
+
 # Create a VPC
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = var.name
+  }
 }
 
 resource "tls_private_key" "this" {
@@ -10,7 +18,7 @@ resource "tls_private_key" "this" {
 module "key_pair" {
   source = "terraform-aws-modules/key-pair/aws"
 
-  key_name   = "deployer-one"
+  key_name   = "deployer-${var.name}"
   public_key = tls_private_key.this.public_key_openssh
 }
 
@@ -38,4 +46,12 @@ resource "aws_s3_bucket" "example" {
   # conflicts.
   bucket = "terraform-getting-started-guide"
   acl    = "private"
+}
+
+output "vpc_id" {
+  value = aws_vpc.example.id
+}
+
+output "aws_eip" {
+  value = aws_eip.ip.public_ip
 }
