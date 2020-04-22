52f12c0a3db4fa7ee01d459b48ad39edba68d41c
Bill Wang <bill.wang>
2020-04-22 13:08:24 +1000

    switch to katacoda imageid

--- terraform-docker/courseBase.sh
+++ terraform-docker/courseBase.sh
@@ -17,8 +17,6 @@ install_zip()
 
 install_zip "terraform" "https://releases.hashicorp.com/terraform/0.12.24/terraform_0.12.24_linux_amd64.zip"
 
-install_zip "packer" "https://releases.hashicorp.com/packer/1.5.1/packer_1.5.1_linux_amd64.zip"
-
 # main
 mkdir -p ~/terraform-docker-demo
 cd ~/terraform-docker-demo
