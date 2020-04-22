a35f827dba3c07461e6189b810abad975b01bd10
Bill Wang <bill.wang>
2020-04-22 17:31:51 +1000

    terraform-aws

--- terraform-aws/assets/main.tf
+++ terraform-aws/assets/main.tf
@@ -14,3 +14,12 @@ resource "aws_eip" "ip" {
     vpc = true
     instance = aws_instance.example.id
 }
+
+# create s3 bucket
+resource "aws_s3_bucket" "example" {
+  # NOTE: S3 bucket names must be unique across _all_ AWS accounts, so
+  # this name must be changed before applying this example to avoid naming
+  # conflicts.
+  bucket = "terraform-getting-started-guide"
+  acl    = "private"
+}
