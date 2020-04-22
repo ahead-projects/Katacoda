80acf530f9e7647d1030d17f487088a47b4f71a1
Bill Wang <bill.wang>
2020-04-22 17:37:20 +1000

    terraform-aws

--- terraform-aws/assets/main.tf
+++ terraform-aws/assets/main.tf
@@ -7,6 +7,10 @@ resource "aws_vpc" "example" {
 resource "aws_instance" "example" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
 }
 
 # Assigning an Elastic IP
